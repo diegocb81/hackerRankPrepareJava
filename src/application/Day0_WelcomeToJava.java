2e89487a521f208136819da033b9be4e157a3740
Diego Barcellos <diegocb81@gmail.com>
2024-04-26 22:59:24 -0300

    Project created and Day 0 OK

---
+++ src/application/Day0_WelcomeToJava.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Day0_WelcomeToJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
