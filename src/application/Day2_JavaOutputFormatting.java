3b2ac2270eccf56b35849147a801f4ee1548fe70
Diego Barcellos <diegocb81@gmail.com>
2024-04-29 23:47:03 -0300

    Day 3 OK

--- src/application/Day2_JavaOutputFormatting.java
+++ src/application/Day2_JavaOutputFormatting.java
@@ -7,11 +7,11 @@ public class Day2_JavaOutputFormatting {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("================================");
-        for(int i=0;i<3;i++) {
-            String s1=sc.next();
-            int x=sc.nextInt();
-            System.out.printf("%-15s%03d%n",s1,x);
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+            System.out.printf("%-15s%03d%n", s1, x);
         }
         System.out.println("================================");
-    } 
+    }
 }
