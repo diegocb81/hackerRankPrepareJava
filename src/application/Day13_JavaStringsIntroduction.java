56e88f7f9a5bd7a2c2487fcc66d881bca4804434
Diego Barcellos <diegocb81@gmail.com>
2024-05-10 01:05:35 -0300

    Day 13 OK

---
+++ src/application/Day13_JavaStringsIntroduction.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class Day13_JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        int len = A.length() + B.length();
+        System.out.println(len);
+
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+            System.out.println(Character.toUpperCase(A.charAt(0)) + A.substring(1) + " " +
+            Character.toUpperCase(B.charAt(0)) + B.substring(1));
+            sc.close();
+        }
+    }
+}
