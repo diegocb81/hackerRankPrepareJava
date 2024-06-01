e4cc29987e2b02a3a786db97f0d6b3e1485fc8a3
Diego Barcellos <diegocb81@gmail.com>
2024-06-01 19:03:22 -0300

    Day 35 - Java Stack

---
+++ src/application/Day35_JavaStack.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Day35_JavaStack {
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			System.out.println(isBalanced(in.next()));
+		}
+		in.close();
+	}
+
+	static boolean isBalanced(String parentheses) {
+		Stack<Character> stack = new Stack<Character>();
+		for (int i = 0; i < parentheses.length(); i++) {
+			char ch = parentheses.charAt(i);
+			if (ch == '(' || ch == '[' || ch == '{') {
+				stack.push(ch);
+			} else if (stack.empty()) {
+				return false;
+			} else {
+				char top = stack.pop();
+				if ((top == '(' && ch != ')') || (top == '[' && ch != ']')
+						|| (top == '{' && ch != '}')) {
+					return false;
+				}
+			}
+		}
+		return stack.empty();
+	}
+}
